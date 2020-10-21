c280f80a5e2c7e8b08b975d91d37497de7b239d1
DryreL <gokdenizcetintr@gmail.com>
2020-10-21 16:37:09 +0300

    Project Initialized

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+
+    Rigidbody _rigidbody;
+    float movementX, movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_rigidbody.AddForce(movementX, movementY, )
+    }
+}
